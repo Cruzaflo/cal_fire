89e9ae1983c17c90283cf6762262c5298a2e8de0
Cruzaflo <64674430+Cruzaflo@users.noreply.github.com>
2020-09-20 18:06:08 -0700

    logic update

---
+++ static/js/logic.js
@@ -0,0 +1,46 @@
+// Store our API endpoint as queryUrl
+var queryUrl = "https://www.fire.ca.gov/umbraco/api/IncidentApi/GeoJsonList?inactive=false.geojson";
+d3.json(queryUrl, function (data){
+  console.log(data)
+})
+
+// //query geojson data 
+// d3.json(queryUrl, function(data){
+//   function onEachFeature (feature, layer){
+//     layer.bindPopup(`<h3>${feature.properties.Name}</h3><hr><p>${new Date(feature.properties.AcresBurned)}</p>`)
+//   }
+
+//   var geoFeatures = data.features
+//   var earthquakes = L.geoJSON(geoFeatures,{
+//     onEachFeature: onEachFeature
+//   })
+
+//   createMap(earthquakes)
+// })
+
+// function createMap(earthquakes) {
+//   var overlayMaps = {
+//     Earthquakes: earthquakes
+//   }
+
+//   var streetMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg?access_token={accessToken}", {
+//     attribution: "cruzaflo",
+//     maxZoom: 18,
+//     id: "mapbox.streets",
+//     accessToken: API_KEY
+//   })
+
+//   var baseMaps = {
+//     TheMAin: streetMap
+//   }
+
+//   var myMap = L.map("map", {
+//     center: [37.09, -95.71],
+//     zoom: 5,
+//     layers: [streetMap, earthquakes]
+//   })
+
+//   L.control.layers(baseMaps, overlayMaps).addTo(myMap)
+
+// }
+
