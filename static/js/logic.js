496fc98126e18990b3b3e20c30718fe43771ad63
Cruzaflo <64674430+Cruzaflo@users.noreply.github.com>
2020-09-21 18:06:45 -0700

    legend (in progress)

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,11 +1,50 @@
 // Store our API endpoint as queryUrl
-var queryUrl = "https://www.fire.ca.gov/umbraco/api/IncidentApi/GeoJsonList?inactive=false.geojson";
-https://opendata.arcgis.com/datasets/4fb94e78686d4932ac71bbe561e7cb9b_0.geojson
-d3.json(queryUrl, function (data){
-  console.log(data)
+//var queryUrl = "https://opendata.arcgis.com/datasets/4fb94e78686d4932ac71bbe561e7cb9b_0.geojson";
+
+// var myMap = L.map("map", {
+//   center: [37.6688, -122.0810],
+//   zoom: 5
+// })
+
+// L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg?access_token={accessToken}", {
+//   attribution: "cruzaflo",
+//   maxZoom: 18,
+//   id: "mapbox.streets",
+//   accessToken: API_KEY
+// }).addTo(myMap)
+
+var myMap = L.map("map", {
+  center: [37.6688, -122.0810],
+  zoom: 13
 })
 
-// //query geojson data 
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data $copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap)
+
+
+
+//d3.json(queryUrl, function (data){
+//   console.log(data)
+//   var myMap = L.map("map", {
+//     center: [37.09, -95.71],
+//     zoom: 5
+//   })
+
+//   L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg?access_token={accessToken}", {
+//     attribution: "cruzaflo",
+//     maxZoom: 18,
+//     id: "mapbox.streets",
+//     accessToken: API_KEY
+//   }).addTo(myMap)
+
+//   L.geoJSON(data.features).addTo(myMap)
+// })
+
+//query geojson data 
 // d3.json(queryUrl, function(data){
 //   function onEachFeature (feature, layer){
 //     layer.bindPopup(`<h3>${feature.properties.Name}</h3><hr><p>${new Date(feature.properties.AcresBurned)}</p>`)
