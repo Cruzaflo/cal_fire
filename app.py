05c83c60f0fc0a5fee8b04885a78b91fc348c3bd
Cruzaflo <64674430+Cruzaflo@users.noreply.github.com>
2020-09-27 16:20:34 -0700

    Index
    Added index to root for github

--- app.py
+++ app.py
@@ -9,14 +9,5 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@api.route("/api")
-def api():
-    return "Welcome to Cal Fire API"
-
-@app.route('/api/test')
-def test(): 
-    df = pd.read_csv('./data/test.csv')
-    return df.to_json()
-
 if __name__ == '__main__':
     app.run(debug=True)
